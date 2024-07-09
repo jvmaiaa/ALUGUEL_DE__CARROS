ffe87c5e6f581fbd5246f28010bd69c59fe02a21
jvmaiaa <joaovictormaia482@gmail.com>
2024-07-08 23:29:34 -0300

    Feat: Implementa documentação personalizada com OpenApi

---
+++ src/main/java/com/jvmaiaa/aluguelcarros/api/config/openapi/SpringDocConfig.java
@@ -0,0 +1,39 @@
+package com.jvmaiaa.aluguelcarros.api.config.openapi;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Api de Locação de carros")
+                        .version("v1")
+                        .description("Api que busca realizar a gestão de um locadora, tal como seus veículos, funcionários, e clientes.")
+                        .license(new License()
+                                .name("SpringDoc")
+                                .url("https://springdoc.org")
+                        ))
+                .externalDocs(new ExternalDocumentation()
+                        .description("LinkedIn")
+                        .url("https://www.linkedin.com/in/joão-víctor-maia-4b9961265/"))
+                .tags(
+                        Arrays.asList(
+                                new Tag().name("Carro").description("Operações disponíveis para Carro."),
+                                new Tag().name("Funcionário").description("Operações disponívei para Funcionário."),
+                                new Tag().name("Endereço").description("Operações disponívei para Endereço."),
+                                new Tag().name("Cliente").description("Operações disponívei para Cliente.")
+                        )
+                );
+    }
+}
